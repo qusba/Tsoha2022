81221d450436234dd66ef7a6e228faa2d09958a3
qusba <kasper.kahkonen@outlook.com>
2022-02-07 13:20:26 +0200

    debugging for heroku

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
-INSERT INTO users (username,password,admin) VALUES ('ylläpitäjä','pbkdf2:sha256:260000$61e7Vu6U6BSyiSfS$f2f9c724ab1c10141034a6d1d82f608ab7a1a5e06dede24e942e9967c4af82a8',TRUE);
+INSERT INTO users (username,password,admin) VALUES ('ylläpitäjä',pbkdf2:sha256:260000$61e7Vu6U6BSyiSfS$f2f9c724ab1c10141034a6d1d82f608ab7a1a5e06dede24e942e9967c4af82a8,TRUE);
 CREATE TABLE restaurants (id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE restaurantInfo (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, title TEXT, info TEXT, visible BOOLEAN);
 CREATE TABLE reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, stars INTEGER, review TEXT, visible BOOLEAN, username TEXT, time TIMESTAMP);
